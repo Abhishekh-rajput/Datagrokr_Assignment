c3bf5ad0dce4b9b1ace65aeded313d8026805fc5
Ashutosh1999 <ashutoshkhajuria07@gmail.com>
2020-08-08 09:07:23 +0530

    origin master

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cors = require('cors');
+const path = require('path');
 
 // .env
 const dotenv = require('dotenv');
@@ -44,9 +45,17 @@ app.use((err, req, res, next) => {    // see express-jwt docs
   if (err.name === 'UnauthorizedError') res.status(401).json({error: 'Unauthorized!'});
 });
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'));
-}
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`SUCCESSFULLY LISTENING ON PORT ${port}...`));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+const port=process.env.PORT||5000;
+app.listen(port,()=>{
+    console.log(`server started on  ${port}`);
+})
+
+
